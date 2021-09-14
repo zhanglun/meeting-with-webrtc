3a9b52d5346e079fc4fb10660f6eebe250f78961
zhanglun <zhanglun1410@gmail.com>
2021-09-14 23:19:50 +0800

    feat: get device media

---
+++ client/get-device-media/index.js
@@ -0,0 +1,19 @@
+function app (){
+  const constraints = {
+    video: {
+      width: 1280,
+      height: 720,
+    },
+    // audio: true,
+  }
+
+  const $video = document.querySelector('#video');
+
+  navigator.mediaDevices.getDisplayMedia(constraints)
+    .then((stream) => {
+      console.log(stream);
+      $video.srcObject = stream;
+    });
+}
+
+window.onload = app;
